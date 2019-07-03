752c1b1cf750941f03da5233eaa8b9ee32f4f649
Christopher Davis <chris@christopheralandavis.com>
2019-07-02 22:36:14 -0400

    Initial Commit

---
+++ DesignCodeSwiftUI/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  DesignCodeSwiftUI
+//
+//  Created by Christopher Davis on 7/2/19.
+//  Copyright © 2019 Christopher Davis. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
