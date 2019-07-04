075261c5381da857837ac352f2b197a419d66cb1
Christopher Davis <chris@christopheralandavis.com>
2019-07-04 19:14:38 -0400

    Added the ability to call the certificates view when the profile button is pressed. Added some animations to the view states.

--- DesignCodeSwiftUI/Home.swift
+++ DesignCodeSwiftUI/Home.swift
@@ -11,13 +11,25 @@ import SwiftUI
 struct Home : View {
     
     @State var show = false
+    @State var showProfile = false
     
     var body: some View {
         ZStack {
             
-            Button(action: { self.show.toggle() }) {
-                Text("Open Menu")
-            }
+            ContentView()
+                .background(Color.white)
+                .cornerRadius(30)
+                .shadow(radius: 20)
+                .animation(.fluidSpring())
+                .offset(y: showProfile ? 40 : UIScreen.main.bounds.height)
+            
+            MenuButton(show: $show)
+                .offset(x: -30, y: showProfile ? 0 : 80)
+                .animation(.spring())
+            
+            MenuRight(show: $showProfile)
+                .offset(x: -16, y: showProfile ? 0 : 88)
+                .animation(.spring())
             
             MenuView(show: $show)
         }
@@ -77,19 +89,78 @@ struct MenuView : View {
             }
             
             Spacer()
+            }
+            .padding(.top, 20)
+            .padding(30)
+            .frame(minWidth: 0, maxWidth: .infinity)
+            .background(Color.white)
+            .cornerRadius(30)
+            .padding(.trailing, 60)
+            .shadow(radius: 20)
+            .rotation3DEffect(Angle(degrees: show ? 0 : 60), axis: (x: 0, y: 10.0, z: 0))
+            .animation(.basic())
+            .offset(x: show ? 0 : -UIScreen.main.bounds.width)
+            .tapAction {
+                self.show.toggle()
         }
-        .padding(.top, 20)
-        .padding(30)
-        .frame(minWidth: 0, maxWidth: .infinity)
-        .background(Color.white)
-        .cornerRadius(30)
-        .padding(.trailing, 60)
-        .shadow(radius: 20)
-        .rotation3DEffect(Angle(degrees: show ? 0 : 60), axis: (x: 0, y: 10.0, z: 0))
-        .animation(.basic())
-        .offset(x: show ? 0 : -UIScreen.main.bounds.width)
-        .tapAction {
-            self.show.toggle()
+    }
+}
+
+struct CircleButton : View {
+    
+    var icon: String = "person.crop.circle"
+    
+    var body: some View {
+        return HStack {
+            Image(systemName: icon)
+                .foregroundColor(.black)
+            }
+            .frame(width: 44, height: 44)
+            .background(Color.white)
+            .cornerRadius(30)
+            .shadow(color: Color("buttonShadow"), radius: 10, x: 0, y: 10)
+    }
+}
+
+struct MenuButton : View {
+    
+    @Binding var show: Bool
+    
+    var body: some View {
+        return ZStack(alignment: .topLeading) {
+            Button(action: { self.show.toggle() }) {
+                HStack {
+                    Spacer()
+                    Image(systemName: "list.dash")
+                        .foregroundColor(.black)
+                    }
+                    .padding(.trailing, 20)
+                    .frame(width: 90.0, height: 60.0)
+                    .background(Color.white)
+                    .cornerRadius(30)
+                    .shadow(color: Color("buttonShadow"), radius: 10, x: 0, y: 10)
+            }
+            Spacer()
+        }
+    }
+}
+
+struct MenuRight : View {
+    
+    @Binding var show: Bool
+    
+    var body: some View {
+        return ZStack(alignment: .topTrailing) {
+            HStack {
+                Button(action: { self.show.toggle() }) {
+                    CircleButton(icon: "person.crop.circle")
+                }
+                
+                Button(action: { self.show.toggle() }) {
+                    CircleButton(icon: "bell")
+                }
+            }
+            Spacer()
         }
     }
 }
